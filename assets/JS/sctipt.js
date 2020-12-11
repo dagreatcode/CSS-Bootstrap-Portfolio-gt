fbb1ee941c626c6cc608192cfd1e08a4cb36bc70
dagreatcode <dagreatcode@gmail.com>
2020-12-10 19:52:31 -0500

    working on a JS script for form

---
+++ assets/JS/sctipt.js
@@ -0,0 +1,7 @@
+// var name = nameInput.value;
+// var email = emailInput.value;
+// var message = messageInput.value;
+
+// console.log("Name:", name);
+// console.log("Email:", email);
+// console.log("Message", message);
