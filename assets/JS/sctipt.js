f9bbf541bfe7691775ef7ae4dacc8ff282f0d178
dagreatcode <dagreatcode@gmail.com>
2020-12-19 13:27:00 -0500

    updates

--- assets/JS/sctipt.js
+++ assets/JS/sctipt.js
@@ -1,6 +1,7 @@
 // var name = nameInput.value;
 // var email = emailInput.value;
 // var message = messageInput.value;
+// var submitButtonElement = document.getElementById("submit");
 
 // console.log("Name:", name);
 // console.log("Email:", email);
